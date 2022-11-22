c2bcbfa25b9c14f41bb0cc074fc4d4553f1c18e0
BriWins <95152254+BriWins@users.noreply.github.com>
2022-11-22 10:45:29 -0600

    implement modal

--- JS/index.js
+++ JS/index.js
@@ -36,7 +36,7 @@ function addListItem(pokemon) {
 
 // showDetails() displays pokemon details upon user click
 function showDetails(pokemon) {
-    loadDetails(pokemon). then(function() {
+    loadDetails(pokemon).then(function() {
         console.log(pokemon); 
     })
 }
@@ -64,16 +64,71 @@ function loadList() {
 }
 
 // loadDetails() takes pokemon item as an argument to display its detailed data
-function loadDetails() {
+function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then( function(response) {
         return response.json();
     }).then (function (details) {
-        item.imageUrl = details.sprites.front_default;
+        item.frontImageUrl = details.sprites.front_default;
+        item.backImageUrl = details.sprites.back_default;
         item.types = details.types
     }).catch (function (e) {
         console.error(e);
     });
+}
+
+// showModal() allows each pokemon to display in a modal pop up
+function showModal( title, text ) {
+    let modalContainer = document.querySelector("#modal-container");
+
+    // clears all existing modal content
+    modalContainer.innerHTML = "";
+
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    //add the new modal content
+    let closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = "Close";
+    closeButtonElement.addEventListener("click", hideModal);
+
+    let titleElement = document.createElement("h1");
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement("p");
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add("is-visible");
+
+    modalContainer.addEventListener("click", (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    })
+}
+
+document.querySelector("#show-modal").addEventListener('click', () => { showModal("Modal Title", "This is the modal content");    
+});
+
+// hideModal() removes the modal if user clicks close button
+function hideModal() {
+    let modalContainer = document.querySelector("#modal-container");
+    modalContainer.classList.remove("is-visible");
+
+    //user can exit by pressing the esc key
+    window.addEventListener("keydown", (e) => {
+        let modalContainer = document.querySelector("#modal-container");
+        if ( e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+            hideModal();
+        }
+    })
 }
   
 // return invokes all functions within the immediately invoked functional expression statement
@@ -85,6 +140,8 @@ return {
     showDetails: showDetails,
     loadList: loadList,
     loadDetails: loadDetails,
+    hideModal: hideModal,
+    showModal: showModal,
 }})();
 
 // prints pokemonList details with message for largest pokemon
